1eb311d74f8fc96eaf65913b169ef9a098938c44
Bruno Maia <nomaia@gmail.com>
2026-03-14 02:46:34 -0300

    fiz name

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                name: nome,
+                nome: nome,
                 link: link
             })
         });
