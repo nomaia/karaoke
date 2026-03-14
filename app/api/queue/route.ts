3868853c5850b885c75ba8466f4ee8553350f7bb
Bruno Maia <nomaia@gmail.com>
2026-03-14 01:57:17 -0300

    parse redis queue

--- app/api/queue/route.ts
+++ app/api/queue/route.ts
@@ -4,8 +4,15 @@ import { kv } from "@vercel/kv";
 export async function GET() {
     const queue = await kv.lrange("queue", 0, -1);
 
+    const parsedQueue = queue.map((item: any) => {
+        if (typeof item === "string") {
+            return JSON.parse(item);
+        }
+        return item;
+    });
+
     return NextResponse.json({
-        queue,
+        queue: parsedQueue,
         current: null,
     });
 }
